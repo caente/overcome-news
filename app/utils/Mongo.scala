11106922b6b6c5c8a282cecb734b6bfb8a5415da
Miguel Angel Iglesias <caente@gmail.com>
2013-05-07 12:19:10 -0400

    removed localhost from mongodb connection

--- app/utils/Mongo.scala
+++ app/utils/Mongo.scala
@@ -9,7 +9,7 @@ import play.modules.reactivemongo.json.collection.JSONCollection
 /** Mongo connection object */
 object Mongo {
   val driver = new MongoDriver
-  val connection = driver.connection(List("localhost:27017","heroku_app15540830:3gmmfhcf3f0kn31gbqet7djesr@ds063177.mongolab.com:63177"))
+  val connection = driver.connection(List("heroku_app15540830:3gmmfhcf3f0kn31gbqet7djesr@ds063177.mongolab.com:63177"))
   val db = connection("heroku_app15540830")
 
   def accessLog: JSONCollection = db.collection[JSONCollection]("accessLog")
