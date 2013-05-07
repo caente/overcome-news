1f531cb2f8580e044e3acae8c6ad2590095e8aee
Miguel Angel Iglesias <caente@gmail.com>
2013-05-07 13:57:14 -0400

    trying the plugin

--- app/Global.scala
+++ app/Global.scala
@@ -3,11 +3,13 @@ import play.api.GlobalSettings
 import utils.Mongo
 import actors.ActorStage
 
+import play.modules.reactivemongo.ReactiveMongoPlugin
+import play.api.Play.current
 object Global extends GlobalSettings {
 
   override def onStart(application: play.api.Application) { 
     ActorStage.tweetClientSupervisor ! AddTopic("metal")
   } 
    
-  override def onStop(application: play.api.Application) { Mongo.connection.close() }
+//  override def onStop(application: play.api.Application) { Mongo.connection.close() }
 }
